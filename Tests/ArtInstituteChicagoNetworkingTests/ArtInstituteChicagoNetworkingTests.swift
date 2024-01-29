a9af0c976ca1aaf94cd4549ed4a4f197274254a4
Denis Kulakov <kulakovdenis@mail.ru>
2024-01-29 20:22:29 +0300

    comment  '@available'

--- Tests/ArtInstituteChicagoNetworkingTests/ArtInstituteChicagoNetworkingTests.swift
+++ Tests/ArtInstituteChicagoNetworkingTests/ArtInstituteChicagoNetworkingTests.swift
@@ -6,6 +6,6 @@ final class ArtInstituteChicagoNetworkingTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(ArtInstituteChicagoNetworking().text, "Hello, World!")
+//        XCTAssertEqual(ArtInstituteChicagoNetworking.text, "Hello, World!")
     }
 }
